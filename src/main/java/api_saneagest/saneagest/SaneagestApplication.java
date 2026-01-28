94e0ffad934eefd63220a44ec3e0f452de9a3de8
unknown <ruandasilvasantos49@gmail.com>
2026-01-28 11:39:41 -0300

    setup enviroment

---
+++ src/main/java/api_saneagest/saneagest/SaneagestApplication.java
@@ -0,0 +1,13 @@
+package api_saneagest.saneagest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaneagestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaneagestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api_saneagest/saneagest/SaneagestApplicationTests.java
@@ -0,0 +1,13 @@
+package api_saneagest.saneagest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaneagestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
